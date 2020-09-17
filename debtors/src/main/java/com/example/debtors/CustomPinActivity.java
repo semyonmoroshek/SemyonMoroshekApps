a795e7283254e0c50d95f2044b48f5e1d4137f76
semyonmoroshek <s.moroseks@gmail.com>
2020-09-17 16:33:09 +0300

    added debtors modul

---
+++ debtors/src/main/java/com/example/debtors/CustomPinActivity.java
@@ -0,0 +1,20 @@
+package com.example.debtors;
+
+import com.github.omadahealth.lollipin.lib.managers.AppLockActivity;
+
+public class CustomPinActivity extends AppLockActivity {
+    @Override
+    public void showForgotDialog() {
+
+    }
+
+    @Override
+    public void onPinFailure(int attempts) {
+
+    }
+
+    @Override
+    public void onPinSuccess(int attempts) {
+
+    }
+}
---
+++ debtors/src/main/java/com/example/debtors/DebtorListMainActivity.java
@@ -0,0 +1,37 @@
+package com.example.debtors;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.omadahealth.lollipin.lib.managers.AppLockActivity;
+import com.github.omadahealth.lollipin.lib.managers.LockManager;
+
+public class DebtorListMainActivity  extends AppLockActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debtor_list_main);
+
+        LockManager<CustomPinActivity> lockManager = LockManager.getInstance();
+        lockManager.enableAppLock(this, CustomPinActivity.class);
+
+
+    }
+
+    @Override
+    public void showForgotDialog() {
+
+    }
+
+    @Override
+    public void onPinFailure(int attempts) {
+
+    }
+
+    @Override
+    public void onPinSuccess(int attempts) {
+
+    }
+}
